9473928bb5ab10a917c1c86a8fc16d612fe53192
Christian Imhorst <christian.imhorst@gmail.com>
2017-07-14 16:03:27 +0200

    Saturday

--- saturday/sat_custom.js
+++ saturday/sat_custom.js
@@ -56,6 +56,9 @@ function copyright() {
 			}
 			document.getElementById("Themen").innerHTML = themen;
 			
+			// Terminal-Box
+			var currentLocation = window.location;
+			document.getElementById("Terminal").innerHTML = '<p>Speaker</p>';
 		}); // loadJSON
 	}); // addEventListener
 }()); // function
