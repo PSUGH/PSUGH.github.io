6e47377e6d338517669cf62d192c053845096ffe
Christian Imhorst <christian.imhorst@gmail.com>
2018-05-07 12:50:24 +0200

    Update contact.js

--- contact/contact.js
+++ contact/contact.js
@@ -4,7 +4,7 @@ $(function () {
 
     $('#contact-form').on('submit', function (e) {
         if (!e.isDefaultPrevented()) {
-            var url = "https://datenteiler.de/contact.php";
+            // var url = "https://datenteiler.de/contact.php";
 
             $.ajax({
                 type: "POST",
