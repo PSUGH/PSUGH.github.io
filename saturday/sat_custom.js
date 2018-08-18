55d20aa93297fe518041ccf7b724837b377a9395
Christian Imhorst <christian.imhorst@gmail.com>
2018-08-18 22:39:02 +0200

    Update sat_custom.js

--- saturday/sat_custom.js
+++ saturday/sat_custom.js
@@ -57,7 +57,7 @@ function copyright() {
 			// Terminal-Box
 			var currentLocation = window.location;
 			document.getElementById("Terminal").innerHTML = '<p><strong>Tobias Weltner</strong>, Microsoft MVP für PowerShell</p>' +
-			'<p><strong>Peter Kirchner</strong>, Technical Evangelist bei Microsoft</p>' +
+			'<p><strong>Jan-Henrik Damaschke</strong>, MVP und Senior Consultant bei der Bright Skies GmbH </p>' +
 			'<p><strong>Thorsten Butz</strong>, MCT, Consultant, Autor, Podcaster</p>';
 		}); // loadJSON
 	}); // addEventListener
